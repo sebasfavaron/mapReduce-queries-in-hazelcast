5c0fe18c8cc8eec2216cf2da1a283712fb571a3f
Sebastian Favaron <sebastianfavaron@gmail.com>
2019-10-16 10:43:38 -0300

    Initial commit

---
+++ server/src/main/java/itba/server/Server.java
@@ -0,0 +1,12 @@
+package itba.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("mapReduce Server Starting ...");
+    }
+}
